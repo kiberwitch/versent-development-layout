6bdbe2b44411b976b7dc759a5d7b0d902f67eef8
Murad Bendaliyev <murrrrrrrrrrka33@gmail.com>
2026-01-25 15:21:42 +0400

    Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,html}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				inter: ['Inter', 'sans-serif'],
+				montserrat: ['Montserrat', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
 }
